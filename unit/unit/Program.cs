a065e9887fb4a7cfa9020ac3bca3264fa7531bc5
Strikster <160029365+Strikster@users.noreply.github.com>
2024-11-14 15:28:26 +0300

    Add files via upload

---
+++ unit/unit/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Unit
+{
+   
+    public string Name { get; }
+    private float health; 
+    public int Damage { get; } 
+    private float armor; 
+
+
+    public Unit() : this("Unknown Unit") 
+    {
+       
+    }
+
+    public Unit(string name) 
+    {
+        Name = name; 
+
+        health = 100f; 
+        Damage = 5;    
+        armor = 0.6f;  
+    }
+
+
+    public float Health
+    {
+        get { return health; }
+    }
+
+   
+    public float GetRealHealth()
+    {
+        return Health * (1f + Armor);
+    }
+
+   
+    public float Armor
+    {
+        get { return armor; }
+    }
+
+ 
+    public bool SetDamage(float value)
+    {
+        health -= value * Armor; 
+
+        
+        return health <= 0f;
+    }
+}
